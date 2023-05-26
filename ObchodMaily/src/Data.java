21d2d1df4d0992298cdd39590683029c51546d04
Kuric Christián <christian.kuric@gmail.com>
2023-05-26 08:35:20 +0200

    add

---
+++ ObchodMaily/src/Data.java
@@ -0,0 +1,55 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+public class Data {
+    public static void main(String[] args) {
+
+        try {
+            File inputFile = new File("C:\\Users\\ChristianKu\\Desktop\\BratislavaCourses.my");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("Course");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+
+                    String status = eElement.getElementsByTagName("STATUS").item(0).getTextContent();
+                    String pobocka = eElement.getElementsByTagName("POBOCKA").item(0).getTextContent();
+                    String kod = eElement.getElementsByTagName("KOD").item(0).getTextContent();
+                    String datum = eElement.getElementsByTagName("DATUM").item(0).getTextContent();
+
+                    if ("Bratislava".equals(pobocka) && "OK".equals(status)) {
+                        // Parse the date and check if it is more than 14 days in the future
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+                        LocalDate date = LocalDate.parse(datum.split("-")[1].trim(), formatter);
+
+                        if (LocalDate.now().until(date, ChronoUnit.DAYS) > 10) {
+                            // Check if the date falls within a 6-day range
+                            if (LocalDate.now().until(date, ChronoUnit.DAYS) <= 15) {
+                                System.out.println("KOD: " + kod +" "+ "Datum: "+datum);
+                            }
+                        }
+
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
